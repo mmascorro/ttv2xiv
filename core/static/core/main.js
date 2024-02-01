3019e18486445771974d5a2e1bb4430adf1e5509
Miguel Mascorro <futuretuesday@gmail.com>
2024-01-31 22:37:50 -0600

    email button added

--- core/static/core/main.js
+++ core/static/core/main.js
@@ -1,21 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-  
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-  
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-  
-      });
-    });
-  
+//from bulma navbar example 
+const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+navbarBurgers.forEach( el => {
+  el.addEventListener('click', () => {
+    const targetId = el.dataset.target;
+    const target = document.getElementById(targetId);
+    el.classList.toggle('is-active');
+    target.classList.toggle('is-active');
   });
+});
+
+
+document.getElementById('contact-button').addEventListener('click', e => {
+  const addr = "e`m`.`n`o`t`o`r`p`@`v`i`x`2`v`t`t" .split('`').reverse().join('');
+  console.log(addr)
+  window.location.href = `mailto:${addr}`
+})
